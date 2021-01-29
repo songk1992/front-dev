6cc0d6496ae29ca9c1bb0c1cebd026818f83d703
songk1992 <zuppiy@naver.com>
2021-01-29 15:27:37 +0900

    탭박스 완성

--- ch07/src/main/webapp/js/tabbox.js
+++ ch07/src/main/webapp/js/tabbox.js
@@ -1,24 +1,40 @@
-  
-// DOM Level 2 Event 처리 : element의 event listener를 등록하는 방식
-// 특정 이벤트 핸들러(Listener)를 여러개 등록할 수 있다.  
+
+
 const TabBox = {
-	onWindowLoad: function(){
-		let divTabBox = document.getElementsByClassName('tab-box')[0];
-		let ulTabBox = divTabBox.childNodes[1];
-		let liTabs = ulTabBox.getElementsByTagName('li');
-		Array.prototype.forEach.call(liTabs, function(li){
-			li.addEventListener('click', TabBox.onTabClicked);
-		});
-	},
-	onTabClicked: function(){
-		// unselect
-		let liSelecteds = document.getElementsByClassName('selected');
-		(liSelecteds.length == 1) && (liSelecteds[0].className = '');
-		
-		// select
-		this.className = "selected";
-	},	
 	init: function(){
-		window.addEventListener('load', TabBox.onWindowLoad);		
-	}	
+		$(this._init);
+	},
+	_init: function(){
+		$('.tab-box li').click(TabBox.onTabClicked);
+		TabBox._changeTab();
+	},
+	
+	onTabClicked: function(){
+	// unselect
+	$('.selected').toggleClass('selected');
+	
+	// select
+	let $liTab = (this == TabBox) ? $('.tab-box li:first-child') : $(this);
+	console.log($liTab.attr('no'));
+	$('.ta-box div').text($liTab.data('no')+"의 탭뷰입니다.");
+	$(this).toggleClass("selected");
+	
+	}
+
+
 }
+
+
+
+
+let currentTab;
+$( function() {
+	$('ul li').click(function(){
+		if(currentTab){
+			currentTab.toggleClass("selected");
+		}
+
+		 $(this).toggleClass("selected");
+		 currentTab = $(this);
+	})
+} );  
